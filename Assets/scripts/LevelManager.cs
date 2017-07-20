9d3d2effee1e86f210a85b3168d1a344bb42d393
internetseekho <internetseekho.com@gmail.com>
2017-07-20 18:29:55 +0500

    Code you can use for this section 4
    You can use this code for Code you can use for this section 4 Guess the
    number we are builing on UI

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel( string name ) {
+
+        print( "Load Level " + name );
+
+    }
+
+    public void QuitRequest() {
+
+        print("Quit request received");
+
+    }
+}
